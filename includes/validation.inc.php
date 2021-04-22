b651cd26583ce986ba0342a1163af45856ed5d23
UrosStan <puantpre@gmail.com>
2021-04-23 00:41:14 +0200

    gotova validacija

--- includes/validation.inc.php
+++ includes/validation.inc.php
@@ -1,12 +1,32 @@
 <?php
 
-function emptyInput($fullname,$email,$phone_number,$faculty, $grade, $attend_why){
+function emptyInput($fullname,$email,$phone_number,$faculty, $grade, $attend_why,$radionica1,$radionica2,$radionice_experience,$radionice_why){
 
     if(empty($fullname) || empty($email) || empty($phone_number) || empty($faculty) || empty($grade) || empty($attend_why)){
         return false;
     }
+
+    if(!empty($radionica1) || !empty($radionica2)){
+        if(empty($radionice_experience) || empty($radionice_why) ){
+            return false;
+        }
+    }
     else return true;
 }
+function invalidNumber($phone_number){
+    if (preg_match($phone_number,"/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/")) {
+        return true;
+    }
+    else return false;
+}
+function emptyCv($speed_dating,$cv){
+    if(isset($speed_dating) && $cv==null){
+            return false;
+        
+    }
+    return true;
+}
+
 
 function invalidEmail($email){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
